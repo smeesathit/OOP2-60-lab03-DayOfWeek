e109dc2d5765afdc6f97068bddbff9cdd2bded7c
smeesathit <smeesathit.gmail.com>
2018-01-08 23:19:21 +0700

    Development step 2 : show day of week - complete Add test case in readme.txt
    Signed-off-by: smeesathit <smeesathit.gmail.com>

--- DOW_Finder.java
+++ DOW_Finder.java
@@ -2,6 +2,7 @@
  * This program get birth day and find day of week of that date
  */
 import java.util.Scanner;
+import java.text.SimpleDateFormat;
 
 public class DOW_Finder
 {
@@ -10,14 +11,24 @@ public class DOW_Finder
      */
     public static void main(String[] args)
     {
-        // Get input
+        SimpleDateFormat sdf;
+        // Get input via the standard input window
         String input;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter birthdate (yyyy-mm-dd) : ");
         input = scanner.next();
-        
+
+        /*
         // Echo the input for testing purpose
         System.out.println();
         System.out.println("birthdate = " + input);        
+         */
+
+        // Find and show day of week
+        // Convert String to java.util.Date 
+        java.util.Date bDate = java.sql.Date.valueOf(input);
+        // Show only day of week (full word)
+        sdf = new SimpleDateFormat("EEEE");
+        System.out.println("Your was born on a " + sdf.format(bDate) + ".");        
     } // End main
 } // End class
